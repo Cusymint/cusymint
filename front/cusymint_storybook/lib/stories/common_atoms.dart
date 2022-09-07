c4e70700ad9ed98fcb1f36fcbf077a213d562b23
Dawid Wysocki <tortillazhawaii@gmail.com>
2022-09-07 21:44:12 +0200

    Update Storybook

--- front/cusymint_storybook/lib/stories/common_atoms.dart
+++ front/cusymint_storybook/lib/stories/common_atoms.dart
@@ -10,23 +10,43 @@ class CommonAtoms extends StorybookPart {
   List<Story> get stories => [
         Story(
           name: 'Atoms/TextField',
-          builder: (context) => CuTextField(
-            prefixIcon: context.knobs.nullable.options(
-              label: 'PrefixIcon',
-              initial: const Icon(Icons.ac_unit),
-              options: [],
-            ),
-            suffixIcon: context.knobs.nullable.options(
-              label: 'SuffixIcon',
-              initial: const Icon(
-                Icons.close,
+          builder: (context) => SizedBox(
+            width: 350,
+            child: CuTextField(
+              prefixIcon: context.knobs.nullable.options(
+                label: 'PrefixIcon',
+                initial: const Icon(Icons.ac_unit),
+                options: [],
+              ),
+              suffixIcon: context.knobs.nullable.options(
+                label: 'SuffixIcon',
+                initial: const Icon(
+                  Icons.close,
+                ),
               ),
             ),
           ),
         ),
         Story(
           name: 'Atoms/Card',
-          builder: (context) => const CuCard(),
+          builder: (context) => const SizedBox(
+            width: 150,
+            height: 100,
+            child: CuCard(
+              child: Center(
+                child: CuText('Hello world!'),
+              ),
+            ),
+          ),
+        ),
+        Story(
+          name: 'Atoms/Text',
+          builder: (context) => CuText(
+            context.knobs.text(
+              label: 'Text',
+              initial: 'Hello world!',
+            ),
+          ),
         ),
       ];
 }
