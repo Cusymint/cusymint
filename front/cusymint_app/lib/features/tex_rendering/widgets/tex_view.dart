ceca940f83e03276fb99eec315651faca521f1fc
Dawid Wysocki <tortillazhawaii@gmail.com>
2022-09-12 16:52:06 +0200

    Add tex_view

---
+++ front/cusymint_app/lib/features/tex_rendering/widgets/tex_view.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_math_fork/flutter_math.dart';
+
+/// Wrapper for a third party Tex rendering widget.
+///
+/// List of supported commands:
+/// https://katex.org/docs/supported.html
+class TexView extends StatelessWidget {
+  const TexView(
+    this.data, {
+    super.key,
+    this.fontScale,
+  });
+
+  final String data;
+  final double? fontScale;
+
+  @override
+  Widget build(BuildContext context) {
+    return Math.tex(
+      data,
+      textScaleFactor: fontScale,
+    );
+  }
+}
