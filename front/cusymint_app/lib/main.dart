a59490012bd588fcb18f67ffc32d18299360ad28
Dawid Wysocki <tortillazhawaii@gmail.com>
2022-09-16 12:11:30 +0200

    Add l10n

--- front/cusymint_app/lib/main.dart
+++ front/cusymint_app/lib/main.dart
@@ -1,8 +1,11 @@
-import 'package:cusymint_app/features/home/pages/home_page.dart';
+import 'package:cusymint_app/features/l10n/pages/l10n_example_page.dart';
+import 'package:cusymint_l10n/cusymint_l10n.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const CusymintApp());
+void main() async {
+  await CuL10n.ensureInitialized();
+
+  runApp(const CuL10n(child: CusymintApp()));
 }
 
 class CusymintApp extends StatelessWidget {
@@ -10,9 +13,12 @@ class CusymintApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      locale: context.locale,
+      supportedLocales: context.supportedLocales,
+      localizationsDelegates: context.localizationDelegates,
       title: 'cusymint',
-      home: HomePage(),
+      home: const L10nExamplePage(),
     );
   }
 }
