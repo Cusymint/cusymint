bc7dd94d085870804d5a84643798616e79230a48
Dawid Wysocki <tortillazhawaii@gmail.com>
2022-09-21 13:30:54 +0200

    Merge branch 'master' into experimental/l10n

---
+++ front/cusymint_app/lib/features/tex_rendering/pages/example_integrals_page.dart
@@ -0,0 +1,58 @@
+import 'package:cusymint_app/features/tex_rendering/tex_rendering.dart';
+import 'package:flutter/material.dart';
+
+class ExampleIntegralsPage extends StatelessWidget {
+  const ExampleIntegralsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const scale = 2.0;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: const [
+            TexView(
+              r'''\int_{a}^{b} x^2 \, dx''',
+              fontScale: scale,
+            ),
+            TexView(
+              r'''\int_{a}^{b} e^x + x^{x^{x^{x^x}}} - \sqrt[3]{15x} \, dx''',
+              fontScale: scale,
+            ),
+            Divider(),
+            TexView(
+              r'''\int(x^{2} + x - 1) \, dx''',
+              fontScale: scale,
+            ),
+            TexView(
+              r'''\int e^{2x} \, dx''',
+              fontScale: scale,
+            ),
+            TexView(
+              r'''''',
+              fontScale: scale,
+            ),
+            TexView(
+              r'''\int \frac{1}{x} \, dx''',
+              fontScale: scale,
+            ),
+            TexView(
+              r'''\int \frac{1}{x^2} \, dx''',
+              fontScale: scale,
+            ),
+            TexView(
+              r'''\int \sin^2(x) \, dx''',
+              fontScale: scale,
+            ),
+            Divider(),
+            TexView(
+              r'''\int \ln \frac{\tg{x^{\arctg{x}}}}{\sqrt{\cos x}} \, dx''',
+              fontScale: scale,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
