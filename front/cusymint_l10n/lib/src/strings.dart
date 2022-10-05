214cdd4233d8d56656d6264f91771337d88a4f9f
Dawid Wysocki <tortillazhawaii@gmail.com>
2022-10-05 21:13:00 +0200

    Add Strings class

---
+++ front/cusymint_l10n/lib/src/strings.dart
@@ -0,0 +1,8 @@
+class Strings {
+  const Strings();
+
+  static const String enterIntegral = 'enterIntegral';
+  static const String tryExamples = 'tryExamples';
+  static const String foundResult = 'foundResult';
+  static const String encounteredErrors = 'encounteredErrors';
+}
