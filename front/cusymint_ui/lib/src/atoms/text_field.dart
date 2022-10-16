be5e747b65c8f00ebf67b39cbe898436d948a46d
Dawid Wysocki <dwysocki157@gmail.com>
2022-10-16 23:14:35 +0200

    Add text field style

--- front/cusymint_ui/lib/src/atoms/text_field.dart
+++ front/cusymint_ui/lib/src/atoms/text_field.dart
@@ -1,3 +1,4 @@
+import 'package:cusymint_ui/cusymint_ui.dart';
 import 'package:flutter/material.dart';
 
 class CuTextField extends StatelessWidget {
@@ -20,17 +21,41 @@ class CuTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: apply app style
+    final colors = CuColors.of(context);
+
+    // TODO: fix icon colors
     return TextField(
       controller: controller,
       onTap: onTap,
       onChanged: onChanged,
+      style: TextStyle(
+        color: colors.black,
+        fontSize: 18.0,
+      ),
       decoration: InputDecoration(
         prefixIcon: prefixIcon,
         suffixIcon: suffixIcon,
-        border: const OutlineInputBorder(),
+        prefixIconColor: colors.mintDark,
+        suffixIconColor: colors.mintDark,
         label: label,
+        border: _CuInputBorder(color: colors.mintDark),
+        enabledBorder: _CuInputBorder(color: colors.mintDark),
+        focusedBorder: _CuInputBorder(color: colors.mintHeavyish),
+        errorBorder: _CuInputBorder(color: colors.errorColor),
+        focusColor: colors.mintDark,
+        filled: true,
+        fillColor: colors.mintLight,
+        iconColor: colors.mintDark,
       ),
     );
   }
+}
+
+class _CuInputBorder extends OutlineInputBorder {
+  _CuInputBorder({
+    required CuColor color,
+  }) : super(
+          borderRadius: BorderRadius.circular(8.0),
+          borderSide: BorderSide(color: color, width: 2.0),
+        );
 }
