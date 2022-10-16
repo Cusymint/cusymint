3c2c316c2f3462230961e9e092e3b1988525e6e1
Dawid Wysocki <dwysocki157@gmail.com>
2022-10-16 21:33:03 +0200

    Add colors

--- front/cusymint_ui/lib/src/styleguide/colors.dart
+++ front/cusymint_ui/lib/src/styleguide/colors.dart
@@ -6,13 +6,37 @@ class CuColor extends Color {
 
 class CuColors {
   CuColors({
+    required this.mint,
+    required this.mintLight,
+    required this.mintDark,
+    required this.mintHeavyish,
+    required this.mintGray,
+    required this.errorColor,
     required this.textWhite,
     required this.textBlack,
-    required this.backgroundHighlight,
-    required this.background,
     required this.white,
     required this.black,
   });
+
+  factory CuColors.light() {
+    return CuColors(
+      mint: CuColor(0xFFB9DED2),
+      mintLight: CuColor(0xFFFCFFFE),
+      mintDark: CuColor(0xFF002F20),
+      mintHeavyish: CuColor(0xFF00744A),
+      mintGray: CuColor(0xFF49816F),
+      errorColor: CuColor(0xFF9D4529),
+      textWhite: CuColor(0xFFFFFFFF),
+      textBlack: CuColor(0xFF000000),
+      white: CuColor(0xFFFFFFFF),
+      black: CuColor(0xFF000000),
+    );
+  }
+
+  static CuColors of(BuildContext context) {
+    // TODO: implement using InheritedWidget
+    return CuColors.light();
+  }
 
   final CuColor white;
   final CuColor black;
@@ -20,6 +44,11 @@ class CuColors {
   final CuColor textWhite;
   final CuColor textBlack;
 
-  final CuColor backgroundHighlight;
-  final CuColor background;
+  final CuColor mint;
+  final CuColor mintLight;
+  final CuColor mintDark;
+  final CuColor mintHeavyish;
+  final CuColor mintGray;
+
+  final CuColor errorColor;
 }
