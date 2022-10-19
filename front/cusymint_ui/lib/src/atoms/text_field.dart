b75dc642ece2c9118375a7e1467f4c9285d3f0ae
Dawid Wysocki <dwysocki157@gmail.com>
2022-10-19 21:21:43 +0200

    Add TextField shadow

--- front/cusymint_ui/lib/src/atoms/text_field.dart
+++ front/cusymint_ui/lib/src/atoms/text_field.dart
@@ -1,4 +1,5 @@
 import 'package:cusymint_ui/cusymint_ui.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CuTextField extends StatelessWidget {
@@ -23,7 +24,6 @@ class CuTextField extends StatelessWidget {
   Widget build(BuildContext context) {
     final colors = CuColors.of(context);
 
-    // TODO: add shadow
     return Material(
       type: MaterialType.transparency,
       child: TextField(
@@ -54,11 +54,109 @@ class CuTextField extends StatelessWidget {
   }
 }
 
-class _CuInputBorder extends OutlineInputBorder {
-  _CuInputBorder({
-    required CuColor color,
-  }) : super(
-          borderRadius: BorderRadius.circular(8.0),
-          borderSide: BorderSide(color: color, width: 2.0),
+class _CuInputBorder extends _DecoratedInputBorder {
+  _CuInputBorder({required this.color})
+      : super(
+          child: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: BorderSide(color: color, width: 2.0),
+          ),
+          shadow: BoxShadow(
+            offset: const Offset(0, 4),
+            spreadRadius: 0,
+            blurRadius: 4,
+            color: Colors.black.withOpacity(0.25),
+          ),
         );
+
+  final CuColor color;
+}
+
+// https://stackoverflow.com/questions/54194347/how-to-add-drop-shadow-to-textformfield-in-flutter
+class _DecoratedInputBorder extends InputBorder {
+  _DecoratedInputBorder({
+    required this.child,
+    required this.shadow,
+  }) : super(borderSide: child.borderSide);
+
+  final InputBorder child;
+
+  final BoxShadow shadow;
+
+  @override
+  bool get isOutline => child.isOutline;
+
+  @override
+  Path getInnerPath(Rect rect, {TextDirection? textDirection}) =>
+      child.getInnerPath(rect, textDirection: textDirection);
+
+  @override
+  Path getOuterPath(Rect rect, {TextDirection? textDirection}) =>
+      child.getOuterPath(rect, textDirection: textDirection);
+
+  @override
+  EdgeInsetsGeometry get dimensions => child.dimensions;
+
+  @override
+  InputBorder copyWith(
+      {BorderSide? borderSide,
+      InputBorder? child,
+      BoxShadow? shadow,
+      bool? isOutline}) {
+    return _DecoratedInputBorder(
+      child: (child ?? this.child).copyWith(borderSide: borderSide),
+      shadow: shadow ?? this.shadow,
+    );
+  }
+
+  @override
+  ShapeBorder scale(double t) {
+    final scaledChild = child.scale(t);
+
+    return _DecoratedInputBorder(
+      child: scaledChild is InputBorder ? scaledChild : child,
+      shadow: BoxShadow.lerp(null, shadow, t)!,
+    );
+  }
+
+  @override
+  void paint(Canvas canvas, Rect rect,
+      {double? gapStart,
+      double gapExtent = 0.0,
+      double gapPercentage = 0.0,
+      TextDirection? textDirection}) {
+    final clipPath = Path()
+      ..addRect(const Rect.fromLTWH(-5000, -5000, 10000, 10000))
+      ..addPath(getInnerPath(rect), Offset.zero)
+      ..fillType = PathFillType.evenOdd;
+    canvas.clipPath(clipPath);
+
+    final Paint paint = shadow.toPaint();
+    final Rect bounds = rect.shift(shadow.offset).inflate(shadow.spreadRadius);
+
+    canvas.drawPath(getOuterPath(bounds), paint);
+
+    child.paint(canvas, rect,
+        gapStart: gapStart,
+        gapExtent: gapExtent,
+        gapPercentage: gapPercentage,
+        textDirection: textDirection);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (other.runtimeType != runtimeType) return false;
+    return other is _DecoratedInputBorder &&
+        other.borderSide == borderSide &&
+        other.child == child &&
+        other.shadow == shadow;
+  }
+
+  @override
+  int get hashCode => Object.hash(borderSide, child, shadow);
+
+  @override
+  String toString() {
+    return '${objectRuntimeType(this, 'DecoratedInputBorder')}($borderSide, $shadow, $child)';
+  }
 }
