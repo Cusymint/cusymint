7a284c88502343da62d9d7b31407e46dc8ba7c1c
Szymon Zyguła <szymon@zygula.eu>
2022-11-05 21:33:23 +0100

    Add example test made possible by solution tree collapsing

--- engine/test/test.cu
+++ engine/test/test.cu
@@ -5,15 +5,22 @@
 
 #include "Parser/Parser.cuh"
 
-class Fixture : public ::testing::Test {
+class IntegralCalculation : public ::testing::Test {
   protected:
-    Fixture() { Sym::Static::init_functions(); }
+    IntegralCalculation() { Sym::Static::init_functions(); }
 };
 
-TEST(Tests, Test) { EXPECT_EQ(1, 1 + 1 - 1); }
-
-TEST_F(Fixture, CusymintTest) {
-    const auto integral = Sym::integral(parse_function("x"));
-    EXPECT_EQ("SubexpressionVacancy{ solved by 1 }",
-              Sym::solve_integral(integral).value()[0].data()->to_string());
+void test_integral(const std::string integral_str, const std::string expected_result_str) {
+    const auto integral = Sym::integral(parse_function(integral_str));
+    const auto result = Sym::solve_integral(integral);
+    const auto expected_result = parse_function(expected_result_str);
+    EXPECT_TRUE(result.has_value());
+    EXPECT_TRUE(Sym::Symbol::compare_trees(
+        result.value().data(), // NOLINT(bugprone-unchecked-optional-access)
+        expected_result.data()));
 }
+
+TEST_F(IntegralCalculation, X) { test_integral("x", "0.5*x^2"); }
+TEST_F(IntegralCalculation, EToX) { test_integral("e^x", "e^x"); }
+TEST_F(IntegralCalculation, EToXChain) { test_integral("e^e^x*e^x*e^e^e^x", "e^e^e^x"); }
+TEST_F(IntegralCalculation, CosPlusSin) { test_integral("cos(x)+sin(x)", "sin(x)-cos(x)"); }
