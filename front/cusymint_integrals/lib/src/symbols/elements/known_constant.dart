5280c9e532f4d7df32e8f6b15a06daf8729f5e85
Dawid Wysocki <dwysocki157@gmail.com>
2022-11-07 10:30:45 +0100

    Remove cusymint_integrals

--- front/cusymint_integrals/lib/src/symbols/elements/known_constant.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cusymint_integrals/src/symbols/elements/element.dart';
-
-class EulerConstant extends Element {
-  const EulerConstant() : super(toTexResult: 'e', toUtfResult: 'e');
-}
-
-class PiConstant extends Element {
-  const PiConstant() : super(toTexResult: '\\pi', toUtfResult: 'π');
-}
--- front/cusymint_integrals/lib/src/symbols/elements/variable.dart
+++
@@ -1,5 +0,0 @@
-import 'package:cusymint_integrals/src/symbols/elements/element.dart';
-
-class Variable extends Element {
-  const Variable() : super(toTexResult: 'x', toUtfResult: 'x');
-}
