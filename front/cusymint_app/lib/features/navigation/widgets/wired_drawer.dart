74cc72d6287fb3cbdae1e96af791bda604c7c516
Dawid Wysocki <dwysocki157@gmail.com>
2022-11-07 23:02:39 +0100

    Add WiredDrawer

---
+++ front/cusymint_app/lib/features/navigation/widgets/wired_drawer.dart
@@ -0,0 +1,21 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:cusymint_ui/cusymint_ui.dart';
+
+import '../navigation.dart';
+
+class WiredDrawer extends CuDrawer {
+  WiredDrawer({
+    super.key,
+    required this.context,
+  }) : super(
+          onAboutPressed: () {},
+          onHomePressed: () {
+            context.router.navigate(const WelcomeRoute());
+          },
+          onSettingsPressed: () {
+            context.router.navigate(const SettingsRoute());
+          },
+        );
+
+  final BuildContext context;
+}
---
+++ front/cusymint_app/lib/features/settings/pages/settings_page.dart
@@ -0,0 +1,10 @@
+import 'package:cusymint_ui/cusymint_ui.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
