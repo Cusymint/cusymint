574ca8196800c7abe19763d753e38ce0db8c25e3
Dawid Wysocki <dwysocki157@gmail.com>
2022-11-12 19:28:50 +0100

    Fix Zbędne przechowanie stanu obliczeń #84

--- front/cusymint_app/lib/services_provider.dart
+++ front/cusymint_app/lib/services_provider.dart
@@ -1,9 +1,6 @@
 import 'package:cusymint_app/features/client/client_factory.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
-
-import 'features/home/blocs/client_cubit.dart';
 
 class ServicesProvider extends StatelessWidget {
   const ServicesProvider({super.key, required this.child});
@@ -16,11 +13,6 @@ class ServicesProvider extends StatelessWidget {
       providers: [
         Provider<ClientFactory>(
           create: (context) => ClientFactory(),
-        ),
-        BlocProvider(
-          create: (context) => ClientCubit(
-            clientFactory: ClientFactory.of(context),
-          ),
         ),
       ],
       child: child,
