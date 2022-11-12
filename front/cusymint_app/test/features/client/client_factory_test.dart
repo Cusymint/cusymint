6773ce4dfe8c7d399620a02af9d3b713e2b66e9e
Dawid Wysocki <dwysocki157@gmail.com>
2022-11-12 18:28:42 +0100

    Test ClientFactory

--- front/cusymint_app/test/features/client/client_factory_test.dart
+++ front/cusymint_app/test/features/client/client_factory_test.dart
@@ -1,4 +1,5 @@
 import 'package:cusymint_app/features/client/client_factory.dart';
+import 'package:cusymint_client_json_rpc/cusymint_client_json_rpc.dart';
 import 'package:cusymint_client_mock/cusymint_client_mock.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -21,6 +22,42 @@ void main() {
             (mock) => mock.fakeResponse.outputInUtf,
             'Some output',
             isNotEmpty,
+          ),
+        );
+      });
+
+      test('Mock client with errors', () {
+        final factory = ClientFactory();
+
+        factory.setUrl('errors');
+
+        expect(
+          factory.client,
+          isA<CusymintClientMock>()
+              .having(
+                (mock) => mock.fakeResponse.errors,
+                'Some errors',
+                isNotEmpty,
+              )
+              .having(
+                (mock) => mock.fakeResponse.outputInUtf,
+                'No output',
+                isNull,
+              ),
+        );
+      });
+
+      test('JsonRpc client', () {
+        final factory = ClientFactory();
+
+        factory.setUrl('ws://localhost:8000/websocket');
+
+        expect(
+          factory.client,
+          isA<CusymintClientJsonRpc>().having(
+            (jsonRpc) => jsonRpc.uri,
+            'Correct uri',
+            Uri.parse('ws://localhost:8000/websocket'),
           ),
         );
       });
