230b51bc424584ecd0941c30a6ff72e842c30980
Dawid Wysocki <dwysocki157@gmail.com>
2022-11-25 15:51:43 +0100

    Clean up development pages

--- front/cusymint_app/test/features/tex_rendering/tex_view_test.dart
+++ front/cusymint_app/test/features/tex_rendering/tex_view_test.dart
@@ -1,4 +1,4 @@
-import 'package:cusymint_app/features/tex_rendering/pages/example_integrals_page.dart';
+import 'example_integrals_page.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
 void main() {
