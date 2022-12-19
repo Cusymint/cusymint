5847ed1d8bd39b8f229ca4c8d9d0665827eac2de
zh-qs <szymontur3@o2.pl>
2022-12-19 13:36:07 +0100

    reduce block size for ExpressionArray kernels

--- engine/src/main.cu
+++ engine/src/main.cu
@@ -50,7 +50,7 @@ int main() {
 
     Sym::Static::init_functions();
 
-    const auto integral = Sym::integral(Parser::parse_function("e^x*e^e^x*e^e^e^x+x^2+sin(e^x)*e^x+x^2+x"));
+    const auto integral = Sym::integral(Parser::parse_function("pi*2*e*e^x*10*sin(e^x)*ln(pi)"));
 
     fmt::print("Trying to solve an integral: {}\n", integral.data()->to_tex());
 
