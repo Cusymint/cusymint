a7cced7ed9321c395c5116975bd672776817e281
Szymon Zyguła <szymon@zygula.eu>
2022-12-28 19:18:52 +0100

    Remove 1/x integral

--- engine/src/Evaluation/KnownIntegral/KnownIntegral.cu
+++ engine/src/Evaluation/KnownIntegral/KnownIntegral.cu
@@ -8,18 +8,17 @@
 #include "SimpleExponent.cuh"
 #include "SimpleSineCosine.cuh"
 #include "SimpleVariable.cuh"
-#include "XInverse.cuh"
 
 namespace Sym::KnownIntegral {
     __device__ const Check CHECKS[] = {
         is_simple_variable, is_power_function,    is_simple_exponent, is_simple_sine,
-        is_simple_cosine,   is_constant_integral, is_simple_arctan,   is_x_inverse,
+        is_simple_cosine,   is_constant_integral, is_simple_arctan,
     };
 
     __device__ const Application APPLICATIONS[] = {
         integrate_simple_variable, integrate_power_function, integrate_simple_exponent,
         integrate_simple_sine,     integrate_simple_cosine,  integrate_constant_integral,
-        integrate_simple_arctan,   integrate_x_inverse,
+        integrate_simple_arctan,
     };
 
 #ifdef __CUDA_ARCH__
