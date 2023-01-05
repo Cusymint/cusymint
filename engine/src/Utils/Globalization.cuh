355cf33b55a6ff27d91c2b4a08e40c93257159b3
zh-qs <szymontur3@o2.pl>
2023-01-05 18:26:11 +0100

    Add double square braces

--- engine/src/Utils/Globalization.cuh
+++ engine/src/Utils/Globalization.cuh
@@ -2,18 +2,18 @@
 #define GLOBALIZATION_CUH
 
 namespace Globalization {
-    constexpr char SUBSTITUTE[] = "substitute";
-    constexpr char SPLIT_SUM[] = "splitSum";
-    constexpr char INTEGRATE_BY_PARTS[] = "integrateByParts";
-    constexpr char SOLVE_INTEGRAL[] = "solveIntegral";
-    constexpr char BRING_OUT_CONST[] = "bringOutConstant";
-    constexpr char SIMPLIFY[] = "simplify";
+    constexpr char SUBSTITUTE[] = "[[substitute]]";
+    constexpr char SPLIT_SUM[] = "[[splitSum]]";
+    constexpr char INTEGRATE_BY_PARTS[] = "[[integrateByParts]]";
+    constexpr char SOLVE_INTEGRAL[] = "[[solveIntegral]]";
+    constexpr char BRING_OUT_CONST[] = "[[bringOutConstant]]";
+    constexpr char SIMPLIFY[] = "[[simplify]]";
 
-    constexpr char INTEGRAL[] = "integral";
-    constexpr char ST_SUFFIX[] = "stSuffix";
-    constexpr char ND_SUFFIX[] = "ndSuffix";
-    constexpr char RD_SUFFIX[] = "rdSuffix";
-    constexpr char TH_SUFFIX[] = "thSuffix";
+    constexpr char INTEGRAL[] = "[[integral]]";
+    constexpr char ST_SUFFIX[] = "[[stSuffix]]";
+    constexpr char ND_SUFFIX[] = "[[ndSuffix]]";
+    constexpr char RD_SUFFIX[] = "[[rdSuffix]]";
+    constexpr char TH_SUFFIX[] = "[[thSuffix]]";
 }
 
 #endif
