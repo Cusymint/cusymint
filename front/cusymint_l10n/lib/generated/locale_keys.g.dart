35fb232ca348c27f52a79c8e7465e47aac3250e3
Dawid Wysocki <tortillazhawaii@gmail.com>
2023-01-08 00:26:29 +0100

    Use codegen to create Strings

---
+++ front/cusymint_l10n/lib/generated/locale_keys.g.dart
@@ -0,0 +1,40 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const enterIntegral = 'enterIntegral';
+  static const tryExamples = 'tryExamples';
+  static const foundResult = 'foundResult';
+  static const encounteredErrors = 'encounteredErrors';
+  static const copiedToClipboard = 'copiedToClipboard';
+  static const subtitle = 'subtitle';
+  static const solve = 'solve';
+  static const interpreting = 'interpreting';
+  static const retry = 'retry';
+  static const home = 'home';
+  static const settings = 'settings';
+  static const about = 'about';
+  static const interpretedAs = 'interpretedAs';
+  static const calculating = 'calculating';
+  static const language = 'language';
+  static const ipAddress = 'ipAddress';
+  static const ipAddressError = 'ipAddressError';
+  static const licenses = 'licenses';
+  static const languagePolish = 'languagePolish';
+  static const languageEnglish = 'languageEnglish';
+  static const languageCurrent = 'languageCurrent';
+  static const aboutText = 'aboutText';
+  static const aboutTextGithub = 'aboutTextGithub';
+  static const madeBy = 'madeBy';
+  static const cancel = 'cancel';
+  static const ok = 'ok';
+  static const error = 'error';
+  static const errorUnknown = 'errorUnknown';
+  static const errorUnexpectedEndOfInput = 'errorUnexpectedEndOfInput';
+  static const errorUnexpectedToken = 'errorUnexpectedToken';
+  static const errorNoSolutionFound = 'errorNoSolutionFound';
+  static const errorInternal = 'errorInternal';
+  static const history = 'history';
+  static const historyEmpty = 'historyEmpty';
+  static const clearHistory = 'clearHistory';
+
+}
